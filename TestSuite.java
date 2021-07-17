bee69f03165cbe6db219bda1a991680fcfc1abb5
1592002 <kolla@ualberta.ca>
2021-07-17 13:01:48 -0600

    Firstcommit

--- TestSuite.java
+++ TestSuite.java
@@ -8,8 +8,8 @@ public class TestSuite {
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		//InternetExplorerDriver ie=new InternetExplorerDriver();
-		driver.get("https://google.com");
-		System.out.println(driver.getTitle());
+		//driver.get("https://google.com");
+		//System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
 		System.out.println("going to youtube");
 		driver.get("https://www.aha.video");
